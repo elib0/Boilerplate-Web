091cf53904d9b556b6a340df7e6b31a10b6733f0
elib0 <elijose.c@gmail.com>
2016-01-12 15:34:13 -0430

    Pequeños cambios en GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 var _path = require('path')
-	gulp = require('gulp')
-	less = require('gulp-less'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+  gulp = require('gulp')
+  less = require('gulp-less'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
+  rename = require("gulp-rename");
 
 //Less Plugins
 var LessPluginCleanCSS = require('less-plugin-clean-css'),
     cleancss = new LessPluginCleanCSS({ advanced: true });
 
-//Paths
+//Directorio Publico
 var path = {
 	css: "./public/css",
 	js:	"./public/js"
 };
 
+//Directorio de fuentes
 var directory = {
-	bootstrap: {
-		css: "./node_modules/bootstrap/less/bootstrap.less",
-		js: "./node_modules/bootstrap/js/*.js"
-	}
+  bootstrap: {
+    css: "./node_modules/bootstrap/less/bootstrap.less",
+    js: "./node_modules/bootstrap/js/*.js"
+  },
+  sources:{
+    css: "./sources/less/"
+  }
 };
 
 // Twitter Bootstrap
@@ -26,9 +31,10 @@ gulp.task('bootstrapcss', function () {
   return gulp.src(directory.bootstrap.css)
     .pipe(less({
       plugins: [cleancss],
-      paths: [ _path.join(__dirname, 'less') ],
-      filename: 'bootstrap.less'
-   	}))
+      paths: [ _path.join(__dirname, 'less') ]
+
+    }))
+    .pipe(rename('bootstrap.css'))
     .pipe(gulp.dest(path.css));
 });
 gulp.task('bootstrapjs', function () {
@@ -38,7 +44,24 @@ gulp.task('bootstrapjs', function () {
     .pipe(gulp.dest(path.js));
 });
 gulp.task('bootstrap', ['bootstrapcss', 'bootstrapjs']);
-// END Twitter Bootstrap
+// FIN Twitter Bootstrap
 
-//Default actions for gulp files
-gulp.task('default', ['bootstrap']);
+//Css Propios
+gulp.task('css', function(){
+  return gulp.src(directory.sources.css+'myddoc.less')
+    .pipe(less({
+      plugins: [cleancss]
+    }))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest(path.css));
+});
+// FIN Css Propios
+
+//Ver archivos
+/*var watcher = gulp.watch(directory.sources.css+'*.less', ['css']);
+watcher.on('change', function(event) {
+  console.log('Archivo ' + event.path + ' fue ' + event.type + ', Corriendo Tarea...');
+});*/
+
+//Accion por defecto
+gulp.task('default', ['bootstrap', 'css']);
